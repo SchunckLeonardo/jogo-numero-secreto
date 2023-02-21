0152cbc5af1f697a99d52eb3127aa4657e4f7dbd
Leonardo Schunck Rainha <99510487+SchunckLeonardo@users.noreply.github.com>
2023-02-21 14:31:25 -0300

    Add files via upload

---
+++ app/reconhecimentoVoz.js
@@ -0,0 +1,25 @@
+const elementoChute = document.getElementById("chute")
+
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.lang = 'pt-Br';
+recognition.start()
+
+recognition.addEventListener("result", onSpeak)
+
+function onSpeak(event) {
+    chute = event.results[0][0].transcript;
+    exibirChuteNaTela(chute);
+    verificarChuteValido(chute);
+}
+
+function exibirChuteNaTela(chute) {
+    elementoChute.innerHTML = `
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+    `
+}
+
+recognition.addEventListener("end", () => recognition.start())
